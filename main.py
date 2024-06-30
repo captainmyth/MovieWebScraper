f56fae16b7d9723d070af4b3c63fcb660e84d4db
captainmyth <56376612+captainmyth@users.noreply.github.com>
2024-06-30 18:22:55 -0500

    Update main.py
    Allows the user to input a movie title (e.g. "Shrek") and returns the IMDB Title + Rating. Plans to incorporate Rotten Tomatoes and Metacritic in the future.

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from imdb import Cinemagoer
+
 
 #Fetch IMDB HTML
 def fetch_imdb(movie_title):
-    imdb_website = f'https://www.imdb.com/title/{movie_title}/'
-    imdb_results = requests.get(imdb_website)
-
-    if imdb_results.status_code == 200:
-        return imdb_results.content
+    ia = Cinemagoer()
+    imdb_search = ia.search_movie(movie_title)
+    if imdb_search:
+        for movie in imdb_search:
+            if movie_title.lower() == movie['title'].lower():
+                imdb_movie = ia.get_movie(movie.movieID)
+        return imdb_movie
     else:
-        print(f"Could not find an IMDB page for {movie_title}")
+        print(f'Could not find the IMDB page for {movie_title}')
+
+    
 
 #Fetch Rotten Tomatoes HTML
-def fetch_rotten_tomatoes(movie_title):
+'''def fetch_rotten_tomatoes(movie_title):
     rotten_tomatoes_website = f'https://www.rottentomatoes.com/m/{movie_title.replace(" ", "_")}'
     rotten_tomatoes_results = requests.get(rotten_tomatoes_website)
 
@@ -20,9 +26,9 @@ def fetch_rotten_tomatoes(movie_title):
         return rotten_tomatoes_results.content
     else:
         print(f"Could not find a Rotten Tomatoes page for {movie_title}")
-
+'''
 #Fetch Metacritic HTML
-def fetch_meta_critic(movie_title):
+'''def fetch_meta_critic(movie_title):
     formatted_title = movie_title.lower().replace(" ", "_")
     meta_critic_website = f'https://www.metacritic.com/movie/{formatted_title}'
     meta_critic_results = requests.get(meta_critic_website)
@@ -31,24 +37,19 @@ def fetch_meta_critic(movie_title):
         return meta_critic_results.content
     else:
         print(f"Could not find a Meta Critic page for {movie_title}")
-
+'''
 #Parse IMDB
 def parse_imdb(html):
-    imdb_soup = BeautifulSoup(html, 'html.parser')
-    imdb_title = imdb_soup.find('h1').text.strip()
-    imdb_rating_element = imdb_soup.find('span', itemprop='ratingValue')
-    if imdb_rating_element:
-        imdb_rating = imdb_rating_element.text.strip()
-    else:
-        imdb_rating = 'NA'
-    return ("\nMovie Title:\n" + {imdb_title} + "\nMovie Rating: " + imdb_rating)
+    imdb_title = html.get('title', 'NA')
+    imdb_rating = html.get('rating', 'NA')
+    return imdb_title, str(imdb_rating)
 
-def parse_rotten_tomatoes(html):
+'''def parse_rotten_tomatoes(html):
     rotten_tomatoes_soup = BeautifulSoup(html, 'html.parser')
     rotten_tomatoes_rating = rotten_tomatoes_soup.find('score-board').find('span', class_='mop-ratings-wrap__percentage').text.strip()
     return rotten_tomatoes_rating
-
-def parse_meta_critic(html):
+'''
+'''def parse_meta_critic(html):
     meta_critic_soup = BeautifulSoup(html, 'html.parser')
     meta_critic_element = meta_critic_soup.find('div', class_='metascore_w')
     if meta_critic_element:
@@ -56,35 +57,23 @@ def parse_meta_critic(html):
     else:
         meta_critic_rating = 'NA'
     return meta_critic_rating
-    
+'''    
 def movie_ratings(movie_title):
     imdb_html = fetch_imdb(movie_title)
-    rotten_tomatoes_html = fetch_rotten_tomatoes(movie_title)
-    meta_critic_html = fetch_meta_critic(movie_title)
+    #rotten_tomatoes_html = fetch_rotten_tomatoes(movie_title)
+    #meta_critic_html = fetch_meta_critic(movie_title)
 
     if imdb_html:
         imdb_title, imdb_rating = parse_imdb(imdb_html)
     else:
         imdb_title, imdb_rating = movie_title, 'NA'
-    
-    if rotten_tomatoes_html:
-        rotten_tomatoes_score = parse_rotten_tomatoes(rotten_tomatoes_html)
-    else:
-        rotten_tomatoes_score = 'NA'
-    
-    if meta_critic_html:
-        metascore = parse_meta_critic(meta_critic_html)
-    else:
-        metascore = 'NA'
-    
-
     return {
-        'Title': imdb_title,
-        '\nIMDB Rating': imdb_rating,
-        '\nRotten Tomatoes Rating': rotten_tomatoes_score,
-        '\nMetacritic Score': metascore
+        'Title ': imdb_title,
+        'IMDB Rating ': imdb_rating
     }
+
+    
 
 
 movie_title = input("Please enter a movie title:\n")
-movie_ratings(movie_title)
+print(movie_ratings(movie_title))
